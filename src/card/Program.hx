1bc3fbc90b10892f5382b0851f3eacd3ea71e449
Cédric Belin <cedric@belin.io>
2022-01-16 11:49:33 +0100

    Updated the links section

--- src/card/Program.hx
+++ src/card/Program.hx
@@ -39,7 +39,7 @@ class Program {
 					.center("<light_white>MC2IT</>")
 					.center("<white>Distribution & Services</>")
 				.line()
-					.label("GitHub", "<gray>https://github.com/</><cyan>mc2it</>")
+					.label("Bitbucket", "<gray>https://bitbucket.org/</><cyan>mc2it</>")
 				.line()
 					.label("Card", "<yellow>npx</> <white>@mc2it/card</>")
 					.label("Email", "<white>dev@mc2it.com</>")
