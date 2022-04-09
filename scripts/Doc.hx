e7915325aebd8d33316bc75dcaf49b22c002471e
Cédric Belin <cedric@belin.io>
2022-04-09 19:10:54 +0200

    Updated the API reference

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -12,7 +12,7 @@ function main() {
 		"run", "dox",
 		"--define", "description", "Business card of MC2IT, distribution and services.",
 		"--define", "source-path", "https://github.com/mc2it/card/blob/main/src",
-		"--define", "themeColor", "0xffc105",
+		"--define", "themeColor", "0x165898",
 		"--define", "version", Version.packageVersion,
 		"--define", "website", "https://github.com/mc2it/card",
 		"--input-path", "var",
