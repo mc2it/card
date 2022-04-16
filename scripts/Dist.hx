c18fffa970bf636382283b55aa5c32e8739ed02d
Cédric Belin <cedric@belin.io>
2022-04-16 19:02:54 +0200

    Updated the build system

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -1,5 +1,3 @@
-import sys.io.File;
-
 /** Runs the script. **/
 function main() {
 	for (script in ["Clean", "Version"]) Sys.command('lix $script');
@@ -7,7 +5,6 @@ function main() {
 
 	final file = "bin/mc2it_card.js";
 	Sys.command('npx terser --comments=false --config-file=etc/terser.json --output=$file $file');
-	File.saveContent(file, '#!/usr/bin/env node\n${File.getContent(file)}');
 	Sys.command('git update-index --chmod=+x $file');
 	if (Sys.systemName() != "Windows") Sys.command('chmod +x $file');
 }
