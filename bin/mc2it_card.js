ba5d0f8afa10d2bf8beb7353ee5dd7718ed8840f
Cédric Belin <cedric@belin.io>
2022-07-01 02:42:41 +0200

    Using import assertions for JSON modules

--- bin/mc2it_card.js
+++ bin/mc2it_card.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import console from "node:console";
-import {readFileSync} from "node:fs";
 import {program} from "commander";
+import pkg from "../package.json" assert {type: "json"};
 import {getCard} from "../lib/index.js";
 
 // Start the application.
-const {version} = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf8"));
 program.name("npx @mc2it/card")
 	.description("Print the business card of MC2IT, distribution and services.")
-	.version(version, "-v, --version")
+	.version(pkg.version, "-v, --version")
 	.action(() => console.log(getCard(1)))
 	.parse();
