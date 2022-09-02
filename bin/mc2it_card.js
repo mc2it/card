467c7003b7e57c9c146686e1f40f63f8ec241b16
Cédric Belin <cedric@belin.io>
2022-09-02 10:35:59 +0200

    Dropped the dependency on Commander.js

--- bin/mc2it_card.js
+++ bin/mc2it_card.js
@@ -1,12 +1,30 @@
-#!/usr/bin/env node
 import console from "node:console";
-import {program} from "commander";
+import {parseArgs} from "node:util";
 import pkg from "../package.json" assert {type: "json"};
 import {getCard} from "../src/index.js";
 
+// The usage information.
+const usage = `
+Print the business card of MC2IT, distribution and services.
+
+Usage:
+  npx @mc2it/card
+
+Options:
+  -h, --help     Display this help.
+  -v, --version  Output the version number.
+`;
+
 // Start the application.
-program.name("npx @mc2it/card")
-	.description("Print the business card of MC2IT, distribution and services.")
-	.version(pkg.version, "-v, --version")
-	.action(() => console.log(getCard(1)))
-	.parse();
+try {
+	const {values} = parseArgs({options: {
+		help: {short: "h", type: "boolean"},
+		version: {short: "v", type: "boolean"}
+	}});
+
+	console.log(values.help ? usage.trim() : (values.version ? pkg.version : getCard(1)));
+}
+catch (error) {
+	console.error(error instanceof Error ? error.message : error);
+	process.exit(1);
+}
