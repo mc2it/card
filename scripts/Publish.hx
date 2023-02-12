1b644567d1e5afa5bb7b2cb4d98d2d01c80e3dad
Cédric Belin <cedric@belin.io>
2023-02-12 01:30:59 +0100

    Fixed the "Publish" task

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -4,5 +4,5 @@ import mc2it_card.Version;
 /** Runs the script. **/
 function main() {
 	Sys.command("npm", ["publish"]);
-	for (action in ["tag", "push origin"]) Sys.command("git", [action, 'v${Version.packageVersion}']);
+	for (action in [["tag"], ["push", "origin"]]) Sys.command("git", action.concat(['v${Version.packageVersion}']);
 }
