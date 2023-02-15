a3ab991405279fe786545f226d8fb8cc7e8f92f1
Cédric Belin <cedric@belin.io>
2023-02-15 16:02:22 +0100

    Factorisation de code

--- scripts/Watch.hx
+++ scripts/Watch.hx
@@ -5,15 +5,15 @@ import js.lib.Error as JsError;
 using tink.CoreApi;
 
 /** Runs the script. **/
-function main() GlobWatcher.watch("src/mc2it_card/**/*.hx", {ignoreInitial: false}, done -> measureCallback(done, () -> {
+function main() GlobWatcher.watch("src/mc2it_card/**/*.hx", {ignoreInitial: false}, done -> measureCommand(done, () -> {
 	Sys.command("haxe", ["build.hxml"]);
 	Sys.command("node", ["bin/mc2it_card.js");
 }));
 
-/** Measures the time it takes to run the specified `callback` function. **/
-private function measureCallback(done: Callback<Null<JsError>>, callback: Callback<Noise>) try {
+/** Measures the time it takes to run the specified `command`. **/
+private function measureCommand(done: Callback<Null<JsError>>, command: Callback<Noise>) try {
 	final timestamp = Timer.stamp();
-	callback.invoke(Noise);
+	command.invoke(Noise);
 	Sys.println(Tools.formatDuration(Timer.stamp() - timestamp));
 	done.invoke(null);
 } catch (e) { done.invoke(new JsError(e.message)); }
