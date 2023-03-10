ba847f863f39267f31ff9bbe14eb8c0a1200ace9
Cédric Belin <cedric@belin.io>
2023-03-10 17:00:19 +0100

    Code optimization

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -3,7 +3,7 @@ function main() {
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
 
 	final file = "bin/mc2it_card.js";
-	Sys.command('npx esbuild --allow-overwrite --log-level=warning --minify --outfile=$file --platform=node $file');
+	Sys.command('npx esbuild --allow-overwrite --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
 	Sys.command('git update-index --chmod=+x $file');
 	if (Sys.systemName() != "Windows") Sys.command('chmod +x $file');
 }
