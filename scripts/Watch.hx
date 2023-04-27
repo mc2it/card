c9830880b486e2390b230c20eef18a78a2b5a66e
Cédric Belin <cedric@belin.io>
2023-04-27 17:52:49 +0200

    Code optimization

--- scripts/Watch.hx
+++ scripts/Watch.hx
@@ -8,5 +8,5 @@ function main() GlobWatcher.watch("src/mc2it_card/**/*.hx", {ignoreInitial: fals
 	Sys.command("lix Build --debug");
 	Sys.command("node bin/mc2it_card.js");
 	Sys.println('> ${Tools.formatDuration(Timer.stamp() - timestamp)}');
-	done(null);
+	done();
 });
