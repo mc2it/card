f52b48229b839ebb0358c0cbaebf92f1cf85011d
Cédric Belin <cedric@belin.io>
2023-04-27 16:33:37 +0200

    Update the "Watch" task

--- scripts/Watch.hx
+++ scripts/Watch.hx
@@ -1,20 +1,13 @@
 //! --class-path src --define hxnodejs --library hxnodejs --library tink_core
 import haxe.Timer;
 import js.glob_watcher.GlobWatcher;
-import js.lib.Error as JsError;
 using tink.CoreApi;
 
 /** Watches for file changes. **/
-function main() GlobWatcher.watch("src/mc2it_card/**/*.hx", {ignoreInitial: false}, done -> measureCallback(done, () -> {
+function main() GlobWatcher.watch("src/mc2it_card/**/*.hx", {ignoreInitial: false}, done -> {
+	final timestamp = Timer.stamp();
 	Sys.command("lix Build --debug");
 	Sys.command("node bin/mc2it_card.js");
-}));
-
-/** Measures the time it takes to run the specified `callback` function. **/
-private function measureCallback(?done: Callback<Null<JsError>>, ?prompt: String, callback: Callback<Noise>) try {
-	if (prompt != null) Sys.print('$prompt ');
-	final timestamp = Timer.stamp();
-	callback.invoke(Noise);
 	Sys.println('> ${Tools.formatDuration(Timer.stamp() - timestamp)}');
-	done?.invoke(null);
-} catch (e) { done != null ? done.invoke(new JsError(e.message)) : throw e; }
+	done(null);
+});
