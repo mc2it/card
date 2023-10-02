a25730bcc5b61efc8d02522e3ba2368047d8e4d2
Cédric Belin <cedric@belin.io>
2023-10-02 16:06:58 +0200

    Update the "Dist" task

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -3,4 +3,5 @@ function main() {
 	final file = "bin/mc2it_card.js";
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
 	Sys.command('npx esbuild --allow-overwrite --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
+	Sys.command('git update-index --chmod=+x $file');
 }
