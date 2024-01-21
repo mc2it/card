fa9a935a17e4b755af081ca9e3560bd7ede51595
Cédric Belin <cedric@belin.io>
2024-01-22 00:24:30 +0100

    Port the build system to JS

--- scripts/Publish.hx
+++
@@ -1,8 +0,0 @@
-//! --class-path src
-import mc2it_card.Platform;
-
-/** Publishes the package. **/
-function main() {
-	Sys.command("npm publish");
-	for (action in ["tag", "push origin"]) Sys.command('git $action v${Platform.packageVersion}');
-}
