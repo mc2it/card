1e7c242ffbc20c7469fdd2252d94aafea57e9b3d
Cédric Belin <cedric@belin.io>
2024-01-26 23:00:06 +0100

    Update the build system

--- tool/dist.js
+++ tool/dist.js
@@ -1,6 +1,6 @@
-import {shebang} from "./tools.js";
+import {readFileSync, writeFileSync} from "node:fs";
+import {EOL} from "node:os";
 
-/**
- * Packages the project.
- */
-shebang("bin/mc2it_card.js");
+// Packages the project.
+const file = "bin/mc2it_card.js";
+writeFileSync(file, `#!/usr/bin/env node${EOL}${readFileSync(file, {encoding: "utf8"})}`);
