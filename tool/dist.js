450124d198095075a26647218052239c99679e9a
Cédric Belin <cedric@belin.io>
2024-01-28 12:48:22 +0100

    Optimize the build task

--- tool/dist.js
+++
@@ -1,6 +0,0 @@
-import {readFileSync, writeFileSync} from "node:fs";
-import {EOL} from "node:os";
-
-// Packages the project.
-const file = "bin/mc2it_card.js";
-writeFileSync(file, `#!/usr/bin/env node${EOL}${readFileSync(file, {encoding: "utf8"})}`);
