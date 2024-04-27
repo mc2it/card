34ca4cc299508a036dbd2e33dc6727f64fa2ebff
Cédric Belin <cedric@belin.io>
2024-04-27 17:04:37 +0200

    Fix the "lint" task [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ export async function doc() {
 // Performs the static analysis of source code.
 export async function lint() {
 	await $`tsc --project tsconfig.json`;
-	return $`eslint --config=etc/eslint.config.js gulpfile.js etc src`;
+	return $`eslint --config=etc/eslint.config.js gulpfile.js bin etc src`;
 }
 
 // Publishes the package.
