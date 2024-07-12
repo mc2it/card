75a0b69430eabb2fd11965da7cbe2da9e35ef0f7
Cédric Belin <cedric@belin.io>
2024-07-12 20:15:31 +0200

    Update the TypeScript settings

--- bin/mc2it_card.js
+++ bin/mc2it_card.js
@@ -3,7 +3,7 @@ import console from "node:console";
 import process from "node:process";
 import {parseArgs} from "node:util";
 import pkg from "../package.json" with {type: "json"};
-import {getCard} from "../src/index.js";
+import {getCard} from "../lib/index.js";
 
 // Give the process a friendly name.
 process.title = "MC2IT Card";
