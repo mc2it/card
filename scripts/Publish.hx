61f8c39a8e426fa60d51274b9f60de419b62fee5
Cédric Belin <cedric@belin.io>
2024-10-24 22:00:44 +0200

    Port the build system to Lix scripts

---
+++ scripts/Publish.hx
@@ -0,0 +1,9 @@
+//! --class-path src
+import mc2it_card.Platform;
+
+/** Publishes the package. **/
+function main() {
+	Sys.command("lix Dist");
+	Sys.command("npm publish");
+	for (action in ["tag", "push origin"]) Sys.command('git $action v${Platform.packageVersion}');
+}
---
+++ scripts/Watch.hx
@@ -0,0 +1,45 @@
+//! --class-path src --define hxnodejs --define no-deprecation-warnings --library hxnodejs --library tink_core
+import haxe.Timer;
+import js.glob_watcher.GlobWatcher;
+import js.lib.Error as JsError;
+using DateTools;
+using tink.CoreApi;
+
+/** Watches for file changes. **/
+function main() {
+	measureCommand("lix Build --debug");
+	GlobWatcher.watch("src/mc2it_card/**/*.hx", done -> {
+		measureCommand(done, "haxe --debug build.hxml");
+		Sys.command("node --enable-source-maps bin/mc2it_card.js");
+	});
+}
+
+/** Formats the specified `duration` in seconds. **/
+private function formatDuration(duration: Float) {
+	final operand = Math.pow(10, 3);
+	final timestamp = Math.round(duration * operand) / operand;
+	final seconds = Std.int(timestamp);
+	final milliseconds = Std.int((timestamp - seconds).seconds());
+	return seconds > 1 ? '${seconds}s ${milliseconds}ms' : '${milliseconds}ms';
+}
+
+/** Measures the time it takes to run the specified `command` line. **/
+private function measureCommand(?done: Callback<Null<JsError>>, command: String)
+	measurePromise(done, command, () -> Promise.irreversible((resolve, reject) -> {
+		final exitCode = Sys.command(command);
+		exitCode == 0 ? resolve(Noise) : reject(Error.withData('The command "$command" failed.', exitCode));
+	}));
+
+/** Measures the time it takes to run the specified `promise` generator. **/
+private function measurePromise(?done: Callback<Null<JsError>>, prompt: String, promise: () -> Promise<Any>) {
+	Sys.print('$prompt ');
+	final timestamp = Timer.stamp();
+	promise().handle(outcome -> switch outcome {
+		case Failure(error):
+			Sys.println(error);
+			done?.invoke(error.toJsError());
+		case Success(_):
+			Sys.println('> ${formatDuration(Timer.stamp() - timestamp)}');
+			done?.invoke(null);
+	});
+}
