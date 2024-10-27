9b3a6ecc5c90e81ecc6f834342f44c79e25e85e6
Cédric Belin <cedric@belin.io>
2024-10-27 13:34:52 +0100

    Replace "esbuild" by "terser" for minification

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -2,6 +2,6 @@
 function main() {
 	final file = "bin/mc2it_card.js";
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
-	Sys.command('npx esbuild --allow-overwrite --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
+	Sys.command('npx terser --comments=false --compress --mangle --output=$file $file');
 	Sys.command('git update-index --chmod=+x $file');
 }
