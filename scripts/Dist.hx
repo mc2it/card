8885d6815fa66d5ca49974e43f8341c09810ee29
Cédric Belin <cedric@belin.io>
2024-11-02 23:25:44 +0100

    Port the build system to Cake

--- scripts/Dist.hx
+++
@@ -1,7 +0,0 @@
-/** Packages the project. **/
-function main() {
-	final file = "bin/mc2it_card.js";
-	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
-	Sys.command('npx terser --comments=false --compress --mangle --output=$file $file');
-	Sys.command('git update-index --chmod=+x $file');
-}
--- scripts/Publish.hx
+++
@@ -1,9 +0,0 @@
-//! --class-path src
-import mc2it.card.Platform;
-
-/** Publishes the package. **/
-function main() {
-	Sys.command("lix Dist");
-	Sys.command("npm publish");
-	for (action in ["tag", "push origin"]) Sys.command('git $action v${Platform.packageVersion}');
-}
