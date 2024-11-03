b01f8be0aaae60196fd6f907725725f112e612fd
Cédric Belin <cedric@belin.io>
2024-11-03 13:27:13 +0100

    Update the TS typings [skip ci]

--- lib/card.d.ts
+++ lib/card.d.ts
@@ -1,6 +1,6 @@
 /**
  * Returns the card content.
- * @param {number} margin The margin size.
- * @returns {string} The card content.
+ * @param margin The margin size.
+ * @returns The card content.
  */
 export function getCard(margin?: number): string;
