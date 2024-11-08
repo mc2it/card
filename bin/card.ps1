00380ce77639024452028f47d142c967e240830a
Cédric Belin <cedric@belin.io>
2024-11-08 19:59:02 +0100

    Rename the CLI script

--- bin/card.ps1
+++ bin/card.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
 
-$file = "$PSScriptRoot/mc2it_card.js"
+$file = "$PSScriptRoot/card.js"
 if (Test-Path "$PSScriptRoot/../lib/cli.js.map") { node --enable-source-maps $file @args }
 else { node $file @args }
