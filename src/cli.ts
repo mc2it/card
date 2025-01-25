06266833005253dbed11df4b54edda0dcf0c1f55
Cédric Belin <cedric@belin.io>
2025-01-25 17:44:38 +0100

    Code formatting

--- src/cli.ts
+++ src/cli.ts
@@ -19,7 +19,6 @@ Options:
 // Start the application.
 try {
 	process.title = "MC2IT Card";
-
 	const {values} = parseArgs({options: {
 		help: {short: "h", type: "boolean", default: false},
 		version: {short: "v", type: "boolean", default: false}
