3b2611aeee00dd5e1ead2caa3f4f1f595d05157d
Cédric Belin <cedric@belin.io>
2025-01-28 01:10:06 +0100

    Update the dependencies

--- bin/card.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-
-$file = "$PSScriptRoot/card.js"
-if (Test-Path "$PSScriptRoot/../lib/cli.js.map") { node --enable-source-maps $file @args }
-else { node $file @args }
