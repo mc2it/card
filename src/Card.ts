5f097eca6f76597a5be050fa4c51e1d6972915e3
Cédric Belin <cedric@belin.io>
2025-05-20 16:45:47 +0200

    Add the link to the LinkedIn profile

--- src/Card.ts
+++ src/Card.ts
@@ -8,13 +8,14 @@ import boxen from "boxen";
  */
 export function getCard(margin = 0): string {
 	const buffer = [
-		`${styleText("white", styleText("bold", " MC2IT"))} ${styleText("white", "- Distribution & Services")}`,
+		`${styleText("white", styleText("bold", "  MC2IT"))} ${styleText("white", "- Distribution & Services")}`,
 		"",
-		`${styleText("white", styleText("bold", " GitHub:"))} ${styleText("gray", "https://github.com/")}${styleText("cyan", "mc2it")}`,
+		`${styleText("white", styleText("bold", "  GitHub:"))} ${styleText("gray", "https://github.com/")}${styleText("cyan", "mc2it")}`,
+		`${styleText("white", styleText("bold", "LinkedIn:"))} ${styleText("gray", "https://linkedin.com/company/")}${styleText("cyan", "mc2it-sas")}`,
 		"",
-		`${styleText("white", styleText("bold", "   Card:"))} ${styleText("yellow", "npx")} ${styleText("white", "@mc2it/card")}`,
-		`${styleText("white", styleText("bold", "  Email:"))} ${styleText("white", "dev@mc2it.com")}`,
-		`${styleText("white", styleText("bold", "Website:"))} ${styleText("white", "https://www.mc2it.com")}`
+		`${styleText("white", styleText("bold", "    Card:"))} ${styleText("yellow", "npx")} ${styleText("white", "@mc2it/card")}`,
+		`${styleText("white", styleText("bold", "   Email:"))} ${styleText("white", "dev@mc2it.com")}`,
+		`${styleText("white", styleText("bold", " Website:"))} ${styleText("white", "https://www.mc2it.com")}`
 	];
 
 	return boxen(buffer.join("\n"), {
