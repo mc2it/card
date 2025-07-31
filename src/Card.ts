26df57e95813cd23f862b04dd62f8d48af6bdf61
Cédric Belin <cedx@outlook.com>
2025-07-31 07:52:30 +0200

    Add the postal address and phone number

--- src/Card.ts
+++ src/Card.ts
@@ -9,12 +9,14 @@ import boxen from "boxen";
 export function getCard(margin = 0): string {
 	const buffer = [
 		`${styleText("white", styleText("bold", "  MC2IT"))} ${styleText("white", "- Distribution & Services")}`,
+		"  29 Rue Gine - 34690 Fabrègues",
 		"",
 		`${styleText("white", styleText("bold", "  GitHub:"))} ${styleText("gray", "https://github.com/")}${styleText("cyan", "mc2it")}`,
 		`${styleText("white", styleText("bold", "LinkedIn:"))} ${styleText("gray", "https://linkedin.com/company/")}${styleText("cyan", "mc2it-sas")}`,
 		"",
 		`${styleText("white", styleText("bold", "    Card:"))} ${styleText("yellow", "npx")} ${styleText("white", "@mc2it/card")}`,
 		`${styleText("white", styleText("bold", "   Email:"))} ${styleText("white", "contact@mc2it.com")}`,
+		`${styleText("white", styleText("bold", "   Phone:"))} ${styleText("white", "+33 4 99 52 89 28")}`,
 		`${styleText("white", styleText("bold", " Website:"))} ${styleText("white", "https://www.mc2it.com")}`
 	];
 
