303eb4bef5e89aed365711a2547717c223eb419e
Cédric Belin <cedx@outlook.com>
2025-10-19 17:31:54 +0200

    Update the build system

--- start.ps1
+++ start.ps1
@@ -2,7 +2,4 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
-
-$commandPath = Get-Item $PSCommandPath
-$scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
-node $scriptRoot/bin/Mc2it.Card.js @args
+node $PSScriptRoot/bin/Mc2it.Card.js @args
--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -2,4 +2,4 @@
 Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
 npx tsc --build tsconfig.json --noEmit
-npx eslint --cache --cache-location=var --config=etc/ESLint.js bin src
+npx eslint --cache --cache-location var --config etc/ESLint.js bin src
