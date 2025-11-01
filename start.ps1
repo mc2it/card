cd9a25e206d9756e4003b2992267799e2026b79e
Cédric Belin <cedx@outlook.com>
2025-11-01 13:38:55 +0100

    Update the launch script

--- start.ps1
+++ start.ps1
@@ -2,4 +2,7 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-StrictMode -Version Latest
-node $PSScriptRoot/bin/Mc2it.Card.js @args
+
+$commandPath = Get-Item $PSCommandPath
+$scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
+node $scriptRoot/bin/Mc2it.Card.js @args
