dae493f7c8195d7044bba1a45fbbc2f184d50712
Cédric Belin <cedx@outlook.com>
2025-11-06 16:21:15 +0100

    Code formatting

--- Start.ps1
+++ Start.ps1
@@ -5,5 +5,6 @@ Set-StrictMode -Version Latest
 
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
+
 $package = Get-Content "$scriptRoot/package.json" | ConvertFrom-Json
 node $package.bin.Card @args
