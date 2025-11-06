f2e2aa5ee42b213e58da7fdc57d482669a7aeb70
Cédric Belin <cedx@outlook.com>
2025-11-06 16:15:04 +0100

    Naming convention

--- Start.ps1
+++ Start.ps1
@@ -6,4 +6,4 @@ Set-StrictMode -Version Latest
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
 $package = Get-Content "$scriptRoot/package.json" | ConvertFrom-Json
-node $package.bin.card @args
+node $package.bin.Card @args
