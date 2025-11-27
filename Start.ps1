10def9e1822802f3a9a26a756758c30802130bae
Cédric Belin <cedx@outlook.com>
2025-11-27 10:53:15 +0100

    Update the launch script

--- Start.ps1
+++ Start.ps1
@@ -3,8 +3,5 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-StrictMode -Version Latest
 
-$commandPath = Get-Item $PSCommandPath
-$scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
-
-$package = Get-Content "$scriptRoot/package.json" | ConvertFrom-Json
+$package = Get-Content "$PSScriptRoot/package.json" | ConvertFrom-Json
 node $package.bin.Card @args
